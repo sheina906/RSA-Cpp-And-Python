e26be8d2aa2d9498574b1bd837018e1cfb0bb713
sheina906 <45149382+sheina906@users.noreply.github.com>
2025-04-10 02:25:28 +0300

    support strings
    separating into chunks and reconstructing works good, but not with encryption

--- rsa.cpp
+++ rsa.cpp
@@ -1,38 +1,42 @@
 #include <bitset>
-#define sz 1024
+#include <string>
+#include <vector>
+#include <iostream>
+const int numberSize = 512;
+const int chunkSize = 5;
 using namespace std;
 
 namespace bitsetOperations
 {
-    bitset<sz> operator+(bitset<sz> a, bitset<sz> b);
-    void operator+=(bitset<sz> &a, bitset<sz> b);
-    bitset<sz> operator-(bitset<sz> num);
-    bitset<sz> operator-(bitset<sz> a, bitset<sz> b);
-    void operator-=(bitset<sz> &a, bitset<sz> b);
+    bitset<numberSize> operator+(bitset<numberSize> a, bitset<numberSize> b);
+    void operator+=(bitset<numberSize> &a, bitset<numberSize> b);
+    bitset<numberSize> operator-(bitset<numberSize> num);
+    bitset<numberSize> operator-(bitset<numberSize> a, bitset<numberSize> b);
+    void operator-=(bitset<numberSize> &a, bitset<numberSize> b);
 
-    bitset<sz> operator*(bitset<sz> a, bitset<sz> b);
-    void operator*=(bitset<sz> &a, bitset<sz> b);
+    bitset<numberSize> operator*(bitset<numberSize> a, bitset<numberSize> b);
+    void operator*=(bitset<numberSize> &a, bitset<numberSize> b);
 
-    bitset<sz> operator/(bitset<sz> a, bitset<sz> b);
-    void operator/=(bitset<sz> &a, bitset<sz> b);
-    bitset<sz> operator%(bitset<sz> a, bitset<sz> b);
-    void operator%=(bitset<sz> &a, bitset<sz> b);
+    bitset<numberSize> operator/(bitset<numberSize> a, bitset<numberSize> b);
+    void operator/=(bitset<numberSize> &a, bitset<numberSize> b);
+    bitset<numberSize> operator%(bitset<numberSize> a, bitset<numberSize> b);
+    void operator%=(bitset<numberSize> &a, bitset<numberSize> b);
 
-    void operator%=(bitset<sz> &a, bitset<sz> b);
-    bool operator<(bitset<sz> a, bitset<sz> b);
-    bool operator>(bitset<sz> a, bitset<sz> b);
-    bool operator<=(bitset<sz> a, bitset<sz> b);
-    bool operator>=(bitset<sz> a, bitset<sz> b);
+    void operator%=(bitset<numberSize> &a, bitset<numberSize> b);
+    bool operator<(bitset<numberSize> a, bitset<numberSize> b);
+    bool operator>(bitset<numberSize> a, bitset<numberSize> b);
+    bool operator<=(bitset<numberSize> a, bitset<numberSize> b);
+    bool operator>=(bitset<numberSize> a, bitset<numberSize> b);
 
 
 
     namespace helper
     {
-        pair<bitset<sz>, bitset<sz>> div(bitset<sz> a, bitset<sz> b)
+        pair<bitset<numberSize>, bitset<numberSize>> div(bitset<numberSize> a, bitset<numberSize> b)
         {
-            bitset<sz> remainder = 0, qoutient = 0;
+            bitset<numberSize> remainder = 0, qoutient = 0;
 
-            for (int i = sz-1; i >= 0; i--)
+            for (int i = numberSize-1; i >= 0; i--)
             {
                 remainder <<= 1;
                 if (a[i]) remainder[0] = 1;
@@ -50,31 +54,31 @@ namespace bitsetOperations
 
 
 
-    bitset<sz> operator+(bitset<sz> a, bitset<sz> b)
+    bitset<numberSize> operator+(bitset<numberSize> a, bitset<numberSize> b)
     {
-        bitset<sz> ans = 0;
+        bitset<numberSize> ans = 0;
         bool carry = 0;
-        for (int i = 0; i < sz; i++)
+        for (int i = 0; i < numberSize; i++)
         {
             ans[i] = a[i]^b[i]^carry;
             carry = (a[i]&b[i])|(carry&(a[i]|b[i]));
         }
         return ans;
     }
-    void operator+=(bitset<sz> &a, bitset<sz> b)
+    void operator+=(bitset<numberSize> &a, bitset<numberSize> b)
     {
         a = a + b;
     }
-    bitset<sz> operator-(bitset<sz> num)
+    bitset<numberSize> operator-(bitset<numberSize> num)
     {
         num = ~num;
-        return num + bitset<sz>(1);
+        return num + bitset<numberSize>(1);
     }
-    bitset<sz> operator-(bitset<sz> a, bitset<sz> b)
+    bitset<numberSize> operator-(bitset<numberSize> a, bitset<numberSize> b)
     {
         return a + (-b);
     }
-    void operator-=(bitset<sz> &a, bitset<sz> b)
+    void operator-=(bitset<numberSize> &a, bitset<numberSize> b)
     {
         a = a - b;
     }
@@ -82,14 +86,14 @@ namespace bitsetOperations
 
 
 
-    bitset<sz> operator*(bitset<sz> a, bitset<sz> b)
+    bitset<numberSize> operator*(bitset<numberSize> a, bitset<numberSize> b)
     {
-        bitset<sz> ans = 0;
+        bitset<numberSize> ans = 0;
         if (a.count() > b.count()) swap(a, b);
-        for (int i = 0; i < sz; i++) if (a[i]) ans += (b<<i);
+        for (int i = 0; i < numberSize; i++) if (a[i]) ans += (b<<i);
         return ans;
     }
-    void operator*=(bitset<sz> &a, bitset<sz> b)
+    void operator*=(bitset<numberSize> &a, bitset<numberSize> b)
     {
         a = a*b;
     }
@@ -97,19 +101,19 @@ namespace bitsetOperations
 
 
 
-    bitset<sz> operator/(bitset<sz> a, bitset<sz> b)
+    bitset<numberSize> operator/(bitset<numberSize> a, bitset<numberSize> b)
     {
         return helper::div(a, b).first;
     }
-    void operator/=(bitset<sz> &a, bitset<sz> b)
+    void operator/=(bitset<numberSize> &a, bitset<numberSize> b)
     {
         a = a / b;
     }
-    bitset<sz> operator%(bitset<sz> a, bitset<sz> b)
+    bitset<numberSize> operator%(bitset<numberSize> a, bitset<numberSize> b)
     {
         return helper::div(a, b).second;
     }
-    void operator%=(bitset<sz> &a, bitset<sz> b)
+    void operator%=(bitset<numberSize> &a, bitset<numberSize> b)
     {
         a = a % b;
     }
@@ -117,24 +121,24 @@ namespace bitsetOperations
 
 
 
-    bool operator<(bitset<sz> a, bitset<sz> b)
+    bool operator<(bitset<numberSize> a, bitset<numberSize> b)
     {
-        if (a[sz-1]^b[sz-1]) return a[sz-1];
-        for (int i = sz-2; i >= 0; i--) if (a[i]^b[i]) return b[i];
+        if (a[numberSize-1]^b[numberSize-1]) return a[numberSize-1];
+        for (int i = numberSize-2; i >= 0; i--) if (a[i]^b[i]) return b[i];
         return false;
     }
 
-    bool operator>(bitset<sz> a, bitset<sz> b)
+    bool operator>(bitset<numberSize> a, bitset<numberSize> b)
     {
         return !(a < b) && !(a == b);
     }
 
-    bool operator<=(bitset<sz> a, bitset<sz> b)
+    bool operator<=(bitset<numberSize> a, bitset<numberSize> b)
     {
         return a < b || a == b;
     }
 
-    bool operator>=(bitset<sz> a, bitset<sz> b)
+    bool operator>=(bitset<numberSize> a, bitset<numberSize> b)
     {
         return !(a < b);
     }
@@ -147,31 +151,74 @@ using namespace bitsetOperations;
 
 struct rsa
 {
-    bitset<sz> n, pub, priv;
+    bitset<numberSize> n, pub, priv;
 
-    rsa(bitset<sz> N, bitset<sz> E, bitset<sz> D): n(N), pub(E), priv(D)
+    rsa(bitset<numberSize> N, bitset<numberSize> E, bitset<numberSize> D): n(N), pub(E), priv(D)
     {}
 
-    bitset<sz> encrypt(bitset<sz> msg)
+    bitset<numberSize> encrypt(bitset<numberSize> msg)
     {
         return pwr(msg, pub);
     }
 
-    bitset<sz> decrypt(bitset<sz> enc)
+    bitset<numberSize> decrypt(bitset<numberSize> msg)
     {
-        return pwr(enc, priv);
+        return pwr(msg, priv);
     }
 
 
+
+    vector<bitset<numberSize>> encrypt(string msg)
+    {
+        vector<bitset<numberSize>> encrypted;
+        if (msg.size()%chunkSize != 0) msg += string(chunkSize - (msg.size()%chunkSize), 0);
+        for (int i = 0; i < msg.size(); i += chunkSize)
+        {
+            int ind = 0;
+            bitset<numberSize> chunk;
+            for (int j = 0; j < chunkSize; j++) for (int k = 0; k < 7; k++)  chunk[ind++] = msg[i+j]&(1<<k);
+            encrypted.push_back(encrypt(chunk));
+        }
+        return encrypted;
+    }
+
+    string decrypt(vector<bitset<numberSize>> msg)
+    {
+        string decrypted = "";
+        for (int i = 0; i < msg.size(); i++)
+        {
+            int ind = 0;
+            string temp = string(chunkSize, 0);
+            bitset<numberSize> dec = decrypt(msg[i]);
+            for (int j = 0; j < chunkSize; j++) for (int k = 0; k < 7; k++) if (dec[ind++]) temp[j] ^= (1<<k);
+            decrypted += temp;
+        }
+        while (!decrypted.empty() && decrypted.back() == 0) decrypted.pop_back();
+        return decrypted;
+    }
+
+
+
 private:
-    bitset<sz> pwr(bitset<sz> &a, bitset<sz> &b)
+    bitset<numberSize> pwr(bitset<numberSize> &a, bitset<numberSize> &b)
     {
         if (b.count()==0) return a;
         bool tmp = b[0];
         b >>= 1;
-        bitset<sz> temp = pwr(a, b);
+        bitset<numberSize> temp = pwr(a, b);
         temp = (temp*temp)%n;
         if (tmp) temp = (temp*a)%n;
         return temp;
     }
 };
+
+
+int main()
+{
+    rsa ins(bitset<numberSize>(67072), bitset<numberSize>(3), bitset<numberSize>(44715));
+    string s = "hello";
+    auto v = ins.encrypt(s);
+    s = ins.decrypt(v);
+    cout << s << '\n';
+    return 0;
+}
